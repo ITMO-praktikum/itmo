4156cf7ae3ee67518726c9fe2b9c0c1130173e5c
IVKrylova <ivk.lis@yandex.ru>
2022-04-02 14:25:26 +0300

    feat: code expanding filter-menu for mobile in our projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,10 @@ const filterButtonSpecial = document.querySelector('.filter__item_special');
 const filterButtonNational = document.querySelector('.filter__item_national');
 const filterButtonDevelopment = document.querySelector('.filter__item_development');
 const cardProjectsContainer = document.querySelector('.our-projects__projects-list');
-const filterContainer = document.querySelector('.filter');
+const filterListContainer = document.querySelector('.filter');
+const buttonExpandFilter = document.querySelector('.our-projects__button-chevron-down');
+const filterItemsList = Array.from(filterListContainer.querySelectorAll('.filter__item'));
+const filterContainer = document.querySelector('.our-projects__filter');
 
 // функция создания карточки
 function createCardProject(dataCard, selectorCard) {
@@ -44,9 +47,34 @@ function handleFilterCardProject(buttonName, dataCard, evt) {
 
 // функция добавления стиля выбранному фильтру
 function addStyleFilter(button) {
-	const buttonChecked = filterContainer.querySelector('.filter__item_selected');
+	const buttonChecked = filterListContainer.querySelector('.filter__item_selected');
 	buttonChecked.classList.remove('filter__item_selected');
 	button.classList.add('filter__item_selected');
+}
+
+// обработчик разворачивания фильтров в mobile
+function expandFilter() {
+	filterItemsList.forEach(item => {
+		if (item.closest('.filter__item_hidden')) {
+			item.classList.remove('filter__item_hidden');
+		}
+	});
+	filterContainer.classList.add('our-projects__filter_hidden-bottom');
+	buttonExpandFilter.classList.add('our-projects__button-chevron-down_hidden');
+}
+
+// обработчик выбора фильтра для экранов меньше 768
+function hendleselectFilterByMobile(evt) {
+	const target = evt.target;
+
+	filterItemsList.forEach(item => {
+		if (target !== item) {
+			item.classList.add('filter__item_hidden');
+		}
+	});
+	filterContainer.classList.remove('our-projects__filter_hidden-bottom');
+	buttonExpandFilter.classList.remove('our-projects__button-chevron-down_hidden');
+
 }
 
 // добавление карточек из массива
@@ -63,7 +91,10 @@ const cardProjectsList = new Section({
 cardProjectsList.renderItems();
 
 // фильтрация карточек проектов
-filterButtonAll.addEventListener('click', _ => {
+filterButtonAll.addEventListener('click', evt => {
+	if (document.documentElement.clientWidth < 768) {
+		hendleselectFilterByMobile(evt);
+	}
 	addStyleFilter(filterButtonAll);
 	cardProjectsContainer.innerHTML = '';
 	const cardProjectsList = new Section({
@@ -79,14 +110,29 @@ filterButtonAll.addEventListener('click', _ => {
 	cardProjectsList.renderItems();
 });
 filterButtonSpecial.addEventListener('click', evt => {
+	if (document.documentElement.clientWidth < 768) {
+		hendleselectFilterByMobile(evt);
+	}
 	addStyleFilter(filterButtonSpecial);
 	handleFilterCardProject('special', projectsList, evt);
 });
 filterButtonNational.addEventListener('click', evt => {
+	if (document.documentElement.clientWidth < 768) {
+		hendleselectFilterByMobile(evt);
+	}
 	addStyleFilter(filterButtonNational);
 	handleFilterCardProject('national', projectsList, evt);
 });
 filterButtonDevelopment.addEventListener('click', evt => {
+	if (document.documentElement.clientWidth < 768) {
+		hendleselectFilterByMobile(evt);
+	}
 	addStyleFilter(filterButtonDevelopment);
 	handleFilterCardProject('development', projectsList, evt);
 });
+
+// разворачивание фильтров для mobile
+buttonExpandFilter.addEventListener('click', _ => {
+	expandFilter();
+});
+
