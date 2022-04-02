97417fdca457c72d9904501f9e0a5a1a80c9d238
Mike <77847034+mikesilantev@users.noreply.github.com>
2022-04-02 21:49:00 +0300

    test menu 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,37 @@
-import { f } from 'core-js/modules/_object-dp';
 import './index.css';
-
-//Burder menu
+//Burger menu
 const burgerMenu = {
   burgerBtn: document.querySelector('.header__menu-burger'),
   menuContainer: document.querySelector('.burger-menu'),
   menuCloseBtn: document.querySelector('.burger-menu__close'),
+  submenuTitle: document.querySelector('.burger-menu__submenu-title'),
+  submenuContainer: document.querySelector('.burger-menu__submenu-items'),
+  link: document.querySelectorAll('.burger-menu__link'),
 }
 
+//preventDefault
+const linkArray = Array.from(burgerMenu.link)
+
+function addPreventDefault(e){
+  console.log(e.length)
+  
+  if (e.length == null ) {
+    e.addEventListener('click', (ev) => {
+      ev.preventDefault();
+    })
+  }
+  else {
+    e.forEach((el) => {
+      el.addEventListener('click', (ev) => {
+        ev.preventDefault();
+      })
+    })
+  }
+
+}
+
+addPreventDefault(linkArray);
+addPreventDefault(burgerMenu.submenuTitle);
 
 burgerMenu.burgerBtn.addEventListener('click', () => {
   console.log('Я бургер буттон')
@@ -16,22 +40,12 @@ burgerMenu.burgerBtn.addEventListener('click', () => {
 
 burgerMenu.menuCloseBtn.addEventListener('click', () => {
   console.log('я кнопка закрытия')
-  burgerMenu.menuContainer.classList.toggle('burger-menu_active')
+  burgerMenu.menuContainer.classList.toggle('burger-menu_active');
+  burgerMenu.submenuContainer.classList.remove('burger-menu__submenu-items_active');
 });
 
+burgerMenu.submenuTitle.addEventListener('click', () => {
+  console.log('Я субменю');
+  burgerMenu.submenuContainer.classList.toggle('burger-menu__submenu-items_active');
+})
 
-
-
-//preventDefault
-// const linkArray = Array.from()
-function addPreventDefault(e){
-  e.forEach((el) => {
-    el.addEventListener('click', (ev) => {
-      ev.preventDefault();
-    })
-  })
-}
-
-
-
-// addPreventDefault()
--- src/script/components/BurgerMenu.js
+++ src/script/components/BurgerMenu.js
@@ -1,2 +1,17 @@
-//Const
+// //Const
+// const bugerMenuSelector = '.burger-menu';
+// import { BurgerMenu } from '../script/components/BurgerMenu';
 
+
+// const test = new BurgerMenu(
+//   bugerMenuSelector,
+// );
+
+// export class BurgerMenu {
+//   constructor(menuSelector) {
+//     this._menu = document.querySelector(menuSelector);
+//   }
+//   open() {
+//     console.log(this._menu);
+//   }
+// }
