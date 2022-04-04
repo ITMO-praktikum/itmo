4a32beb1079d9d6a916dc4ba49d699b5b807cdfd
Rybakov-Ilay <ilay106@mail.ru>
2022-04-04 09:54:01 +0300

    feat: add swiper

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,23 @@
-import './index.css';
+import "./index.css";
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: 1.1,
+  spaceBetween: 10,
+  breakpoints: {
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
