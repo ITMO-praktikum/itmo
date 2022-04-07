dec95571ce1a0c8fbf8fa0f5efbfa14f3d89be98
Irina Semenova <micmouse1@yandex.ru>
2022-04-08 01:09:45 +0300

    Block:Research & Education

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,174 @@
 import './index.css';
+//RESEARCH
+import nameImg from '../images/for pop up.png';
+import nameImg2 from '../images/for pop up.png';
+import nameImg3 from '../images/for pop up.png';
+import nameImg4 from '../images/for pop up.png';
+
+//text for popup
+const popupFirstCard = 
+    {
+      title: 'Анализ социальных данных',
+      img: nameImg,
+      alt: 'Люди смотрят в компьютеры',
+      paragrafOne: 'Учебные лаборатории оснащены современным компьютерным оборудованием, техническими средствами защиты информации, программными и программно-аппаратными комплексами средств защиты, разработанными ведущими производителями: Positive Technologies, Infotecs, SearchInform, «Лаборатория ППШ» и других вендоров. Некоторые из лабораторных установок являются уникальными по своим исследовательским задачам.',
+      paragrafTwo: 'Начиная с первого курса бакалавриата, студенты погружаются в область их будущей профессиональной деятельности, активно развивают навыки программирования, исследования защищенности информации в независимости от формы ее представления, проектирования комплексных интегрированных систем защиты информации, проходят различные виды практик.',
+      paragrafThree: 'В состав факультета входит ряд научных лабораторий и центров, в которых проводятся исследования:',
+      listItem1: 'безопасные киберфизические системы;',
+      listItem2: 'разработка средств защиты информации;',
+      listItem3: 'функциональная безопасность беспилотных транспортных средств;',
+      listItem4: 'теория информации и кодирования;',
+      listItem5: 'информационные процессы в Интернет и безопасность виртуального пространства;',
+      listItem6: 'интеллектуальные защищенные системы мультиагентного взаимодействия и пр.'
+    };
+
+  const popupSecondCard = 
+    {
+      title: 'Анализ социальных данных2',
+      img: nameImg2,
+      alt: 'Люди смотрят в компьютеры',
+      paragrafOne: 'Учебные лаборатории оснащены современным компьютерным оборудованием, техническими средствами защиты информации, программными и программно-аппаратными комплексами средств защиты, разработанными ведущими производителями: Positive Technologies, Infotecs, SearchInform, «Лаборатория ППШ» и других вендоров. Некоторые из лабораторных установок являются уникальными по своим исследовательским задачам.',
+      paragrafTwo: 'Начиная с первого курса бакалавриата, студенты погружаются в область их будущей профессиональной деятельности, активно развивают навыки программирования, исследования защищенности информации в независимости от формы ее представления, проектирования комплексных интегрированных систем защиты информации, проходят различные виды практик.',
+      paragrafThree: 'В состав факультета входит ряд научных лабораторий и центров, в которых проводятся исследования:',
+      listItem1: 'безопасные киберфизические системы;',
+      listItem2: 'разработка средств защиты информации;',
+      listItem3: 'функциональная безопасность беспилотных транспортных средств;',
+      listItem4: 'теория информации и кодирования;',
+      listItem5: 'информационные процессы в Интернет и безопасность виртуального пространства;',
+      listItem6: 'интеллектуальные защищенные системы мультиагентного взаимодействия и пр.'
+    };
+
+  const popupThirdCard = 
+    {
+      title: 'Анализ социальных данных3',
+      img: nameImg3,
+      alt: 'Люди смотрят в компьютеры',
+      paragrafOne: 'Учебные лаборатории оснащены современным компьютерным оборудованием, техническими средствами защиты информации, программными и программно-аппаратными комплексами средств защиты, разработанными ведущими производителями: Positive Technologies, Infotecs, SearchInform, «Лаборатория ППШ» и других вендоров. Некоторые из лабораторных установок являются уникальными по своим исследовательским задачам.',
+      paragrafTwo: 'Начиная с первого курса бакалавриата, студенты погружаются в область их будущей профессиональной деятельности, активно развивают навыки программирования, исследования защищенности информации в независимости от формы ее представления, проектирования комплексных интегрированных систем защиты информации, проходят различные виды практик.',
+      paragrafThree: 'В состав факультета входит ряд научных лабораторий и центров, в которых проводятся исследования:',
+      listItem1: 'безопасные киберфизические системы;',
+      listItem2: 'разработка средств защиты информации;',
+      listItem3: 'функциональная безопасность беспилотных транспортных средств;',
+      listItem4: 'теория информации и кодирования;',
+      listItem5: 'информационные процессы в Интернет и безопасность виртуального пространства;',
+      listItem6: 'интеллектуальные защищенные системы мультиагентного взаимодействия и пр.'
+    };
+
+  const popupFourthCard = 
+    {
+      title: 'Анализ социальных данных4',
+      img: nameImg4,
+      alt: 'Люди смотрят в компьютеры',
+      paragrafOne: 'Учебные лаборатории оснащены современным компьютерным оборудованием, техническими средствами защиты информации, программными и программно-аппаратными комплексами средств защиты, разработанными ведущими производителями: Positive Technologies, Infotecs, SearchInform, «Лаборатория ППШ» и других вендоров. Некоторые из лабораторных установок являются уникальными по своим исследовательским задачам.',
+      paragrafTwo: 'Начиная с первого курса бакалавриата, студенты погружаются в область их будущей профессиональной деятельности, активно развивают навыки программирования, исследования защищенности информации в независимости от формы ее представления, проектирования комплексных интегрированных систем защиты информации, проходят различные виды практик.',
+      paragrafThree: 'В состав факультета входит ряд научных лабораторий и центров, в которых проводятся исследования:',
+      listItem1: 'безопасные киберфизические системы;',
+      listItem2: 'разработка средств защиты информации;',
+      listItem3: 'функциональная безопасность беспилотных транспортных средств;',
+      listItem4: 'теория информации и кодирования;',
+      listItem5: 'информационные процессы в Интернет и безопасность виртуального пространства;',
+      listItem6: 'интеллектуальные защищенные системы мультиагентного взаимодействия и пр.'
+    };
+
+// constant for popup
+  const popup = document.querySelector ('.popup');
+  const popupTitle = popup.querySelector('.popup__title');
+  const popupImg = popup.querySelector('.popup__img');
+  const popupParagrafOne = popup.querySelector('.popup__paragraf-one');
+  const popupParagrafTwo = popup.querySelector('.popup__paragraf-two');
+  const popupParagrafThree = popup.querySelector('.popup__paragraf-three');
+  const popupItem1 = popup.querySelector('.popup__list-item1');
+  const popupItem2 = popup.querySelector('.popup__list-item2');
+  const popupItem3 = popup.querySelector('.popup__list-item3');
+  const popupItem4 = popup.querySelector('.popup__list-item4');
+  const popupItem5 = popup.querySelector('.popup__list-item5');
+  const popupItem6 = popup.querySelector('.popup__list-item6');
+  const popupButtons = document.querySelectorAll('.research__item-button');
+
+// open & close popup
+  function openedPopup () {
+    popup.classList.add('popup_opened');
+};
+  function closePopup () {
+    popup.classList.remove('popup_opened');
+};
+
+// close popup by overlay & closeButton
+popup.addEventListener('mousedown', (evt) => {
+  if (evt.target.classList.contains('popup_opened')) {
+      closePopup();
+  }
+  if (evt.target.classList.contains('popup__close-button')) {
+      closePopup();
+    };
+});
+
+// popup text
+function render (item) {
+    popupTitle.textContent = item.title;
+    popupImg.src = item.img;
+    popupImg.alt = item.alt;
+    popupParagrafOne.textContent = item.paragrafOne;
+    popupParagrafTwo.textContent = item.paragrafTwo;
+    popupParagrafThree.textContent = item.paragrafThree;
+    popupItem1.textContent = item.listItem1;
+    popupItem2.textContent = item.listItem2;
+    popupItem3.textContent = item.listItem3;
+    popupItem4.textContent = item.listItem4;
+    popupItem5.textContent = item.listItem5;
+    popupItem6.textContent = item.listItem6;
+      }
+
+// click button -> add text popup 
+popupButtons.forEach(function(button){
+button.addEventListener ('click', function(){
+  if (button.classList.contains('research__item-button-one'))
+  {
+    render(popupFirstCard); 
+  };
+  if (button.classList.contains('research__item-button-two'))
+  {
+    render(popupSecondCard); 
+  };
+  if (button.classList.contains('research__item-button-three'))
+  {
+    render(popupThirdCard);
+  };
+  if (button.classList.contains('research__item-button-four'))
+  {
+    render(popupFourthCard); 
+  };
+openedPopup();
+})});
+
+// EDUCATION
+// constant
+const educationContainerNonactive = document.querySelector('.education__container_nonactive');
+const educationButtonMagistr = document.querySelector('.education__button-magistr');
+const educationButtonAspirant = document.querySelector('.education__button-aspirant');
+const educationButton = document.querySelectorAll('.education__button');
+
+//change card content 
+educationButton.forEach(function(button){
+  button.addEventListener ('click', function(){
+    if (button.classList.contains('education__button-magistr'))
+    {
+      educationButtonMagistr.classList.add('education__button_active');
+      educationButtonAspirant.classList.remove('education__button_active');
+      educationContainerNonactive.classList.add('education__container_active');
+    };
+    if (button.classList.contains('education__button-aspirant'))
+    {
+      educationButtonMagistr.classList.remove('education__button_active');
+      educationButtonAspirant.classList.add('education__button_active');
+      educationContainerNonactive.classList.remove('education__container_active');
+    };
+  })});
+
+
+
+  
+
+  
+
+  
