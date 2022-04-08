9d86e45f2ed45ff48dc6b8558df728b2546dad37
Rybakov-Ilay <ilay106@mail.ru>
2022-04-08 21:39:48 +0300

    feat: working version, but needs editing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import "./index.css";
 
 const swiper = new Swiper('.swiper', {
-  // loop: true,
-  slidesPerView: 'auto',
+  loop: true,
+  // slidesOffsetBefore: 80
+  slidesPerView: "auto",
   centeredSlides: true,
-  initialSlide: 0,
+  // initialSlide: 0,
   spaceBetween: 10,
+  breakpoints: {
+    768: {
+      spaceBetween:16,
+      centeredSlides: false,
+    },
+  },
+
   pagination: {
     el: '.swiper-pagination',
   },
